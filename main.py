5b584fdb99faf8c3d3fe75c9e641f9d92d1390b6
Dsuction <da0434395@gmail.com>
2025-02-24 23:41:34 +0500

    refact levels menu

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     window_size = screen.get_size()
     clock = pg.time.Clock()
     scene: Scene = MenuScene(window_size)
+
     while True:
         scene.update()
         scene: Scene  = choice_scene[scene.event](window_size) if scene.event else scene
